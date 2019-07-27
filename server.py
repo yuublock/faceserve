916201234c3ccd7d0f162d678d6dd38b5648e923
yang-zhengyuan <yang.official@gmail.com>
2019-07-27 12:43:26 -0700

    refactored draw box

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ import face_recognition
 from flask import Flask, jsonify, request, redirect, send_file
 from PIL import Image, ImageDraw
 import numpy as np
+from draw_boxes import draw_boxes
 
 # You can change this to any folder on your system
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -86,67 +87,6 @@ def detect_faces_in_image(file_stream):
     }
     # return jsonify(result)
     return send_file("image_with_boxes.jpg", mimetype='image/jpeg')
-
-def draw_boxes(known_image, unknown_image):
-    # Load a sample picture and learn how to recognize it.
-    # known_image = face_recognition.load_image_file("images/known.jpg")
-    known_face_encoding = face_recognition.face_encodings(known_image)[0]
-
-    # Create arrays of known face encodings and their names
-    known_face_encodings = [
-        known_face_encoding
-    ]
-    known_face_names = [
-        "Known"
-    ]
-
-    # Load an image with an unknown face
-    # unknown_image = face_recognition.load_image_file("images/unknown.jpg")
-
-    # Find all the faces and face encodings in the unknown image
-    face_locations = face_recognition.face_locations(unknown_image)
-    face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
-
-    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
-    # See http://pillow.readthedocs.io/ for more about PIL/Pillow
-    pil_image = Image.fromarray(unknown_image)
-    # Create a Pillow ImageDraw Draw instance to draw with
-    draw = ImageDraw.Draw(pil_image)
-
-    # Loop through each face found in the unknown image
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
-        name = "Unknown"
-
-        # If a match was found in known_face_encodings, just use the first one.
-        # if True in matches:
-        #     first_match_index = matches.index(True)
-        #     name = known_face_names[first_match_index]
-
-        # Or instead, use the known face with the smallest distance to the new face
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-
-        # Draw a box around the face using the Pillow module
-        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
-
-        # Draw a label with a name below the face
-        #text_width, text_height = draw.textsize(name)
-        #draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
-        #draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
-        if name == 'Known':
-            draw.rectangle(((left, top), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
-
-    # Remove the drawing library from memory as per the Pillow docs
-    del draw
-
-    pil_image.save("image_with_boxes.jpg")
-    return pil_image
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
